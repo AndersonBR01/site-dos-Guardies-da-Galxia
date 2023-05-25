ef74e84f066779bf8717cd9f568f05d408cfbeb4
Anderson <andersoncsilva850@gmail.com>
2023-05-25 11:06:14 -0300

    Site
    Estilizando o site e colocando as funções nos butoes e menu

--- script/index.js
+++ script/index.js
@@ -11,14 +11,18 @@ const members = [
 let activeMember = 0
 
 const images = document.getElementById("images")
+const menu = document.getElementById("menu")
+const nameText = document.getElementById("member__name")
 
 function changeStatusButtons() {
   
   const prev = document.getElementById("button__prev");
   const isFirst = activeMember === 0
+  prev.disabled = isFirst
 
   const next = document.getElementById("button__next");
   const isLast = activeMember === members.length -1;
+  next.disabled=isLast
   
 }
 
@@ -27,7 +31,14 @@ function navigationMember(direction) {
 
   const member = members[activeMember]
 
-  console.log(activeMember,member);
+  console.log(activeMember, member);
 
-  images.style.transform =`translateY(${-100}vh)`
+  images.style.transform =`translateY(${-100 * activeMember}vh)`
+
+  changeStatusButtons()
+
+}
+
+function changeMenu() {
+  menu.classList.toggle('active')
 }
